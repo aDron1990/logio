9fa2ee9173a183d7fdac61a2cb5272e636c882a5
Andrey Sheveliov <adron1990@vk.com>
2026-03-26 02:27:35 +0300

    exception catch

--- source/main.cpp
+++ source/main.cpp
@@ -1,8 +1,19 @@
 #include "game.hpp"
 
+#include <exception>
+#include <print>
+
 int main(int argc, char* argv[])
 {
-    auto game = Game{};
-    game.run();
-	return 0;
+    try
+    {
+        auto game = Game{};
+        game.run();
+    }
+    catch (std::exception& ex)
+    {
+        std::println("{}", ex.what());
+        return 1;
+    }
+    return 0;
 }
